edfba2313b04109824ce9c1423d22b741fd1735c
Maurice | Momithso <mts.sonntag@gmail.com>
2021-04-26 10:14:42 +0200

    [TSP-9] Finished ball movement after mouse release

--- src/de/game/objects/Ball.java
+++ src/de/game/objects/Ball.java
@@ -2,6 +2,7 @@ package de.game.objects;
 
 import java.util.ArrayList;
 
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.geom.Circle;
@@ -16,13 +17,39 @@ import org.newdawn.slick.geom.Vector2f;
  */
 public class Ball extends GameObject {
 
+	/**
+	 * Needed for rendering
+	 */
 	private static boolean render = false;
+	private static boolean renderBalls = false;
+	
+	/** Input */
 	private final Input input;
 	
+	/** Moving speed of the ball */
+	private float acceleration = 0.25f;
+	
+	/** Position of the ball as vector (DON'T EDIT FOR CHANGING POSITION: USE this.setPosition or this.x, this.y) */
+	private Vector2f position = new Vector2f(x, y);
+	
+	/** First position of the mouse when the left mouse button is pressed as vector */
 	private Vector2f pressedMousePosition = new Vector2f(0, 0);
 	
+	/** Last mouse position of the mouse when the left mouse button is released as vector */
+	private Vector2f shootMousePosition = new Vector2f(0, 0);
+	
+	/** Value added to ball's position for moving */
+	private Vector2f directionVector = new Vector2f(0, 0);
+	
+	/** Saving small balls of the shooting UI */
 	ArrayList<Circle> balls = new ArrayList<Circle>();
 	
+	/**
+	 * Constructor of the player ball
+	 * @param name
+	 * @param input
+	 * @param shape
+	 */
 	public Ball(String name, Input input, Shape shape) {
 		super(name, shape);
 		this.input = input;
@@ -34,48 +61,149 @@ public class Ball extends GameObject {
 		
 	}
 	
-	public void update(int delta) {
+	/**
+	 * Update method
+	 * @param delta
+	 * @param container
+	 */
+	public void update(int delta, GameContainer container) {
 		super.update(delta);
 		
+		position.set(this.x, this.y);
+		
+		/**
+		 * On first mouse press
+		 */
 		if (input.isMousePressed(0)) {
-			this.pressedMousePosition.set(input.getMouseX(), input.getMouseY());
 			
+			/**
+			 * Save mouse position on first click for placing the ball before release
+			 */
+			this.pressedMousePosition.set(input.getMouseX(), input.getMouseY());
 			this.setPosition(this.pressedMousePosition.getX(), this.pressedMousePosition.getY());
 		}
 		
+		/**
+		 * On mouse is pressed
+		 */
 		if (input.isMouseButtonDown(0)) {
+
+			/**
+			 * Drawing shooting UI
+			 */
+			this.calculateShootingUI();
 			
-			Vector2f mousePosition = new Vector2f(input.getMouseX(), input.getMouseY());
+			/**
+			 * Save shooting position to calculate direction
+			 */
+			this.shootMousePosition.set(input.getMouseX(), input.getMouseY());
 			
-			float x = 1f / balls.size();
+			/**
+			 * Calculating the direction vector to set first direction
+			 */
+			this.calculateDirectionVector();
 			
-			for (Circle ball : balls) {
-				
-				Vector2f ballPosition = new Vector2f(
-						(float) (this.pressedMousePosition.getX() + x * (mousePosition.getX() - this.pressedMousePosition.getX())), 
-						(float) (this.pressedMousePosition.getY() + x * (mousePosition.getY() - this.pressedMousePosition.getY()))
-				);
-				
-				ball.setCenterX(ballPosition.getX());
-				ball.setCenterY(ballPosition.getY());
-				
-				x += 1f / balls.size();
-			}
-			
+			/**
+			 * Sets rendering of balls and shooting UI to true
+			 */
 			render = true;
+			renderBalls = true;
 		} else {
-			render = false;
+
+			/**
+			 * Moving the ball after mouse button 0 (left) released
+			 */
+			this.x += acceleration * delta * this.directionVector.getX();
+			this.y += acceleration * delta * this.directionVector.getY();
+			
+			/**
+			 * Sets rendering of shooting UI to false because its unnecessary
+			 */
+			renderBalls = false;
 		}
 		
 	}
 	
-	public void render(Graphics g) {
+	/**
+	 * Render method
+	 * @param g
+	 * @param container
+	 */
+	public void render(Graphics g, GameContainer container) {
+
+		/**
+		 * Render ball
+		 */
 		if (render) {
 			super.render(g);
+		}
+		
+		/**
+		 * Render shooting UI
+		 */
+		if (renderBalls) {
 			for (Circle ball: balls) {
 				g.fill(ball);
 			}
 			
+		}
+	}
+	
+	/**
+	 * Calculating the direction vector that is required to move the ball
+	 */
+	private void calculateDirectionVector() {
+		
+		/**
+		 * Calculate shooting angle in degrees (360°)
+		 */
+		float angle = (float) Math.toDegrees(
+				Math.atan2(
+						shootMousePosition.getY() - position.getY(), 
+						shootMousePosition.getX() - position.getX()
+				)
+		);
+		
+		/**
+		 * Sets the direction vector as a unit vector
+		 */
+		this.directionVector.set(
+				(float) Math.cos(Math.toRadians(angle)), 
+				(float) Math.sin(Math.toRadians(angle))
+		);
+	}
+	
+	/**
+	 * Calculating the shooting UI
+	 * @param position
+	 */
+	private void calculateShootingUI() {
+		
+		/**
+		 * Calculating parts between starting point and mouse point
+		 */
+		float x = 1f / balls.size();
+		
+		for (Circle ball : balls) {
+			
+			/**
+			 * Using vector straight line equation ( d = p-> + x * v-> )
+			 */
+			Vector2f ballPosition = new Vector2f(
+					(float) (this.pressedMousePosition.getX() + x * (this.shootMousePosition.getX() - this.pressedMousePosition.getX())), 
+					(float) (this.pressedMousePosition.getY() + x * (this.shootMousePosition.getY() - this.pressedMousePosition.getY()))
+			);
+			
+			/**
+			 * Setting position of the ball
+			 */
+			ball.setCenterX(ballPosition.getX());
+			ball.setCenterY(ballPosition.getY());
+			
+			/**
+			 * Adding x by x each time to get closer to mouse position
+			 */
+			x += 1f / balls.size();
 		}
 	}
 }
